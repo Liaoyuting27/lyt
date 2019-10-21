b8311dd888ef5a516c9e07f11852cc5bc7e6b3ca
Liaoyuting27 <33418654+Liaoyuting27@users.noreply.github.com>
2019-10-21 16:01:55 +0800

    Update distributed.py

--- distributed.py
+++ distributed.py
@@ -14,8 +14,8 @@ flags.DEFINE_integer("train_steps",10000,
 flags.DEFINE_integer("batch_size",100, "Training batch size")
 flags.DEFINE_float("learning_rate",0.01, "Learning rate")
 flags.DEFINE_boolean("sync_replicas",False, "True or False")
-flags.DEFINE_integer("replicas_to_aggregate",None,"Number") #多少batch更新一次
-#定义ps的地址
+flags.DEFINE_integer("replicas_to_aggregate",None,"Number") 
+
 flags.DEFINE_string("ps_hosts","192.168.122.223:2222","hostname:port")
 flags.DEFINE_string("worker_hosts","192.168.122.3:2222, 192.168.122.5:2222","hostname:port")
 flags.DEFINE_string("job_name",None, "job name: worker or ps")
@@ -29,7 +29,7 @@ IMAGE_PIXELS = 28
 def main(unused_argv):
     mnist = input_data.read_data_sets(FLAGS.data_dir,one_hot = True)
     
-    #检测命令行输入的参数，确保有job_name和task_index这两个必备参数
+    
     if FLAGS.job_name is None or FLAGS.job_name == "":
         raise ValueError("Must specify an explicit 'job_name'")
     if FLAGS.task_index is None or FLAGS.task_index == "":
@@ -50,14 +50,14 @@ def main(unused_argv):
     is_chief = (FLAGS.task_index==0)
     
     worker_device = "/job:worker/task:%d/cpu:0" % FLAGS.task_index
-    #worker_device计算资源；ps_device存储模型参数资源;tf.device指定模型具体运行设备
+    
     with tf.device(
             tf.train.replica_device_setter(
                     worker_device=worker_device,
                     ps_device = "/job:ps/cpu:0",
                     cluster = cluster)):
         global_step = tf.Variable(0,name="global_step",trainable = False)
-        #记录全局训练步数
+        
         
         hid_w = tf.Variable(
             tf.truncated_normal([IMAGE_PIXELS*IMAGE_PIXELS,FLAGS.hidden_units],
@@ -82,8 +82,7 @@ def main(unused_argv):
     
         opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
     
-    #判断是否设置了同步训练模式sync_replicas,获取所需的副本数
-    #使用tf.train.SyncReplicasOptimizer创建同步训练的优化器
+   
         if FLAGS.sync_replicas:
             if FLAGS.replicas_to_aggregate is None:
                 replicas_to_aggregate = num_workers
@@ -100,19 +99,19 @@ def main(unused_argv):
         train_step = opt.minimize(cross_entropy, global_step=global_step)
     
         if FLAGS.sync_replicas and is_chief:
-            chief_queue_runner = opt.get_chief_queue_runner() #创建队列执行器
-            init_tokens_op = opt.get_init_tokens_op() #创建全局参数初始化器
+            chief_queue_runner = opt.get_chief_queue_runner() 
+            init_tokens_op = opt.get_init_tokens_op() 
         
         init_op = tf.global_variables_initializer()
         train_dir = tempfile.mkdtemp()
-    #创建分布式监督器
+   
         sv = tf.train.Supervisor(is_chief = is_chief,
-                             logdir = logdir, #用来保存模型
+                             logdir = logdir, 
                              init_op = init-op,
                              recovery_wait_secs = 1,
                              global_step = global_step)
     
-    #配置参数
+ 
         sess_config = tf.ConfigProto(
             allow_soft_placement = True,
             log_device_placement = False,
